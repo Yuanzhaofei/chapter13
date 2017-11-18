b7824af9d8071369c3b11d8f7133ee9f275dce02
yuanzhaofei <thyuanzf@hotmail.com>
2017-11-18 15:04:09 +0800

    将HasPtr改为指针类型的类，添加引用计数；

--- test13.22/HasPtr.cpp
+++ test13.22/HasPtr.cpp
@@ -1,22 +1,28 @@
 #include "HasPtr.h"
-
-
-
+//拷贝构造函数
 HasPtr::HasPtr(const HasPtr &hp) :
-	ps(new string(*hp.ps)),
-	i(hp.i)
+	ps(hp.ps),
+	i(hp.i),
+	rcnt(hp.rcnt)
 {
-	//空
+	++*rcnt;//引用计数加1
 }
+//拷贝赋值运算符
 HasPtr&HasPtr::operator=(const HasPtr &hp) {
-	auto newp = new string(*hp.ps);
-	delete ps;//需要释放旧内存！！！
-	ps = newp;
+	++*hp.rcnt;//右边引用计数加1
+	if (--*rcnt == 0) {//左边对象引用计数减1
+		delete ps;//需要释放旧内存！！！
+		delete rcnt;
+	}		
+	ps = hp.ps;
 	i = hp.i;
+	rcnt = hp.rcnt;
 	return *this;
 }
 
-HasPtr::~HasPtr()
-{
-	delete ps;
+HasPtr::~HasPtr(){
+	if (--*rcnt == 0) {//左边对象引用计数减1
+		delete ps;//需要释放旧内存！！！
+		delete rcnt;
+	}
 }
