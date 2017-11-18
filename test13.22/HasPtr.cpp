3fa41727b3ea3ae2dca132bdde520b5dc19008d8
yuanzhaofei <thyuanzf@hotmail.com>
2017-11-18 15:36:02 +0800

    Revert "将HasPtr改为指针类型的类，添加引用计数；"
    This reverts commit 841c83b1f83750a4ef1cc4248614d71f9e783bcf.

--- test13.22/HasPtr.cpp
+++ test13.22/HasPtr.cpp
@@ -1,28 +1,22 @@
 #include "HasPtr.h"
-//拷贝构造函数
+
+
+
 HasPtr::HasPtr(const HasPtr &hp) :
-	ps(hp.ps),
-	i(hp.i),
-	rcnt(hp.rcnt)
+	ps(new string(*hp.ps)),
+	i(hp.i)
 {
-	++*rcnt;//引用计数加1
+	//空
 }
-//拷贝赋值运算符
 HasPtr&HasPtr::operator=(const HasPtr &hp) {
-	++*hp.rcnt;//右边引用计数加1
-	if (--*rcnt == 0) {//左边对象引用计数减1
-		delete ps;//需要释放旧内存！！！
-		delete rcnt;
-	}		
-	ps = hp.ps;
+	auto newp = new string(*hp.ps);
+	delete ps;//需要释放旧内存！！！
+	ps = newp;
 	i = hp.i;
-	rcnt = hp.rcnt;
 	return *this;
 }
 
-HasPtr::~HasPtr(){
-	if (--*rcnt == 0) {//左边对象引用计数减1
-		delete ps;//需要释放旧内存！！！
-		delete rcnt;
-	}
+HasPtr::~HasPtr()
+{
+	delete ps;
 }
--- test13.22/HasPtr.h
+++ test13.22/HasPtr.h
@@ -1,22 +1,15 @@
-#ifndef HASPTR_H
-#define HASPTR_H
+#pragma once
 #include<string>
 using namespace std;
-//定义行为像指针的类HasPtr指向一个string
-//需要引用计数
 class HasPtr
 {
 public:
-	HasPtr(const string & s = string()) :ps(new string(s)), i(0),rcnt(new size_t(1)) {}
+	HasPtr(const string & s=string()):ps(new string(s)),i(0){}
 	~HasPtr();
 	HasPtr(const HasPtr &hp);
 	HasPtr& operator=(const HasPtr &hp);
 private:
-	string *ps;//
-	size_t *rcnt;//引用计数
+	string *ps;
 	int i;
 };
-#endif // HASPTR_H
-
-
 
